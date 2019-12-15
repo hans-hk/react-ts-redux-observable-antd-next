234614021a49457c21e75ba366aa87c8062bc962
hans <leekorea09@gmail.com>
2019-12-15 23:13:00 +0900

    feat: mdx 추가, 간단한 stories 예제 추가

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,17 @@
-module.exports = ({config}) => {
+module.exports = ({baseConfig, env, config}) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
       {
-        loader: require.resolve('awesome-typescript-loader'),
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [['react-app', { flow: false, typescript: true }]]
+        }
       },
-    ],
+      require.resolve('react-docgen-typescript-loader')
+    ]
   });
+
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
--- src/stories/MyComponent.stories.tsx
+++ src/stories/MyComponent.stories.tsx
@@ -1,13 +1,40 @@
+import {decorate} from '@storybook/addon-actions';
+import {boolean, text, withKnobs} from '@storybook/addon-knobs';
 import React from 'react';
 import MyComponent from '../components/MyComponent';
 
+const pick = decorate([args => [args[0].nativeEvent]]);
+const firstArg = decorate([args => args.slice(0, 1)]);
+
 export default {
-  component: 'components|basic/MyComponent',
-  title: 'MyComponent',
+  title: 'components|basic/MyComponent', //어떤 컴포넌트를 문서화 할지 명시
+  component: MyComponent, //스토리북에서 보여질 그룹과 경로를 명시
+  decorators: [withKnobs], //에드온 적용
+  parameters: {
+    componentSubtitle: '하이 난 부제목',
+  },
+};
+
+export const hello = () => {
+  const big = boolean('big', false);
+  const name = text('name', 'Storybook');
+
+  return (
+    <MyComponent
+      name={name}
+      big={big}
+      onHello={pick.action('onHello')}
+      onBye={firstArg.action('onBye')}
+    />
+  );
+};
+
+hello.story = {
+  name: 'hello',
 };
 
 export const myComponent = () => <MyComponent name="Storybook" />;
-export const big = () => <MyComponent name="storybook big" />;
+export const big = () => <MyComponent name="storybook" big />;
 
 myComponent.story = {
   name: 'Default',
